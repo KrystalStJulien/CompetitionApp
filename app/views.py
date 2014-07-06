27d271859996e4ff2a7d1975608e11f688e81dd1
KrystalStJulien <roskiby@gmail.com>
2014-07-05 22:13:32 -0700

    first push with actual app code

--- app/views.py
+++ app/views.py
@@ -1,6 +1,22 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = { 'nickname': 'Krystal' } # fake user
+    return render_template("index.html",
+        title = 'Home',
+        user = user)
+
+@app.route('/new_competition')
+def new_comp():
+    return render_template("new_comp.html")
+
+@app.route('/profile')
+def profile():
+    return render_template("profile.html")
+
+@app.route('/points')
+def points():
+    return render_template("points.html")
